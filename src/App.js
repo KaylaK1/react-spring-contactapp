a90661a4f5b2185aedbbe042d6e7c855ad98cc07
KaylaK1 <44605348+KaylaK1@users.noreply.github.com>
2024-07-27 17:53:02 -0400

    added display contact list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
+import ContactList from './components/ContactList';
 import { getContacts } from './api/ContactService';
 import './App.css';
 
@@ -13,19 +15,34 @@ function App() {
       const { data } = await getContacts(page, size);
       setData(data);
       console.log(data);
-    } catch(error) {
+    } catch (error) {
       console.log(error);
     }
   }
 
-  const toggleModal = (show) => {}
+  const toggleModal = (show) => { }
   useEffect(() => {
     getAllContacts();
   }, []);
-  
+
   return (
     <>
       <Header toggleModal={toggleModal} nbOfContacts={data.totalElements} />
+      <main className='main'>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Navigate to={'/contacts'} />} />
+            <Route path='/contacts' element={
+              <ContactList
+                data={data}
+                currentPage={currentPage}
+                getAllContacts={getAllContacts}
+              />}
+            />
+      </Routes>
+        </div>
+      </main>
+
     </>
   );
 }
